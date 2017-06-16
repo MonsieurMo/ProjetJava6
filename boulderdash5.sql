ac691b4ee8c39354ba6fee8b55e613f1e1fc6c95
Charles Homo <charles.homo@viacesi.fr>
2017-06-16 14:02:52 +0200

    Level 5

---
+++ boulderdash5.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Lun 12 Juin 2017 à 20:25
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `boulderdash`
+--
+
+DELIMITER $$
+--
+-- Procédures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findAllLevels` ()  NO SQL
+SELECT *
+FROM lvl5$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findLvl5ById` (IN `ligne` INT)  NO SQL
+SELECT *
+FROM lvl5
+WHERE lvl5.ligne = id$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findLvl5ByName` (IN `colonne1` varchar(255), 'colonne2' varchar(255), 'colonne3' varchar(255), `colonne4` varchar(255), 'colonne5' varchar(255), 'colonne6' varchar(255), `colonne7` varchar(255), 'colonne8' varchar(255), 'colonne9' varchar(255), `colonne10` varchar(255), 'colonne11' varchar(255), 'colonne12' varchar(255),`colonne13` varchar(255), 'colonne14' varchar(255), 'colonne15' varchar(255),`colonne16` varchar(255), 'colonne17' varchar(255), 'colonne18' varchar(255),`colonne19` varchar(255), 'colonne20' varchar(255), 'colonne21' varchar(255),`colonne22` varchar(255), 'colonne23' varchar(255), 'colonne24' varchar(255), `colonne25` varchar(255), 'colonne26' varchar(255), 'colonne27' varchar(255), `colonne28` varchar(255), 'colonne29'varchar(255),  'colonne30' varchar(255))  NO SQL
+SELECT *
+FROM lvl5
+WHERE lvl5.colonne* = name$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `lvl5`
+--
+
+CREATE TABLE `lvl5` (
+  `ligne` int(11) NOT NULL,
+  `colonne1` varchar(255),
+  `colonne2` varchar(255),
+  `colonne3` varchar(255),
+  `colonne4` varchar(255),
+  `colonne5` varchar(255),
+  `colonne6` varchar(255),
+  `colonne7` varchar(255),
+  `colonne8` varchar(255),
+  `colonne9` varchar(255),
+  `colonne10` varchar(255),
+  `colonne11` varchar(255),
+  `colonne12` varchar(255),
+  `colonne13` varchar(255),
+  `colonne14` varchar(255),
+  `colonne15` varchar(255),
+  `colonne16` varchar(255),
+  `colonne17` varchar(255),
+  `colonne18` varchar(255),
+  `colonne19` varchar(255),
+  `colonne20` varchar(255),
+  `colonne21` varchar(255),
+  `colonne22` varchar(255),
+  `colonne23` varchar(255),
+  `colonne24` varchar(255),
+  `colonne25` varchar(255),
+  `colonne26` varchar(255),
+  `colonne27` varchar(255),
+  `colonne28` varchar(255),
+  `colonne29` varchar(255),
+  `colonne30` varchar(255),
+
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `lvl5`
+--
+
+INSERT INTO `lvl5` (`ligne`, `colonne1`, 'colonne2', 'colonne3', `colonne4`, 'colonne5', 'colonne6', `colonne7`, 'colonne8', 'colonne9', `colonne10`, 'colonne11', 'colonne12',`colonne13`, 'colonne14', 'colonne15',`colonne16`, 'colonne17', 'colonne18',`colonne19`, 'colonne20', 'colonne21',`colonne22`, 'colonne23', 'colonne24', `colonne25`, 'colonne26', 'colonne27', `colonne28`, 'colonne29', 'colonne30') VALUES
+(1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5),
+(2, 5, 1, 1, 1, 1, 1, 0, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 5),
+(3, 5, 1, 8, 1, 1, 1, 3, 1, 5, 1, 1, 1, 4, 1, 1, 1, 1, 1, 5, 1, 1, 4, 1, 1, 1, 1, 5, 1, 3, 5),
+(4, 5, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 3, 1, 1, 5, 0, 1, 5),
+(5, 5, 1, 1, 1, 1, 1, 5, 5, 5, 1, 1, 1, 2, 1, 4, 4, 4, 1, 5, 1, 1, 4, 1, 1, 1, 1, 5, 1, 1, 5),
+(6, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 5, 1, 1, 1, 5, 5, 5, 5, 5, 4, 1, 5),
+(7, 5, 4, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 5, 1, 4, 5),
+(8, 5, 1, 1, 3, 1, 1, 1, 1, 2, 1, 0, 0, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 2, 1, 1, 5 ,1, 1, 5),
+(9, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 5),
+(10, 5, 1, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 1, 1, 1, 1, 1, 1, 5, 1, 1, 5),
+(11, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 5),
+(12, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5),
+(13, 5, 1, 2, 1, 1, 4, 1, 1, 3, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5),
+(14, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 1, 1, 1, 5),
+(15, 5, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5),
+(16, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 5),
+(17, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 4, 1, 0, 0, 5),
+(18, 5, 1, 3, 1, 1, 2, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5),
+(19, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 5),
+(20, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `lvl5`
+--
+ALTER TABLE `lvl5`
+  ADD PRIMARY KEY (`ligne`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `lvl5`
+--
+ALTER TABLE `lvl5`
+  MODIFY `ligne` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
